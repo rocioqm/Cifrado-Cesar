bc6ac454cfe79436c326588bbb0fe5bd3957542c
Rocio <Rocio>
2017-10-10 12:28:47 -0500

    update

--- js/app.js
+++ js/app.js
@@ -8,13 +8,16 @@ function cipher(array){
   //Se crea un prompt para pedirle al usuario que nos ingrese una frase.
   var phrase= prompt("Ingrese una frase");
 
-  //Mediante este "for" recoremos cada letra y luego esas letras las transformaremos en código ASCII mediante "charCodeAt"
+  //Mediante este "for" recoremos cada letra y luego hallaremos l posición de esas letras en el cifrado césar mediante "charCodeAt"
    for (i=0; i<phrase.length;i++){
    
      var acommodation=phrase.charCodeAt(i); 
    }
-
-   //Una vez qu ya tenemos las palabras en código ASCII, es decir en numeros las alacenamos en un array y con eso está concluido el cifrado.
+// Creamos una nueva variable para transformar esas posiciones en cifrado césar a código ASCII.
+ 
+    var acommodation1= (acommodation(i)-65+33)%26+65
+    
+   //Una vez que ya tenemos las palabras en código ASCII, es decir en numeros las alacenamos en un array y con eso está concluido el cifrado.
   for (j=0; j<acommodation.length;j++){
 
     var newPhrase=acommodation.push(j);
